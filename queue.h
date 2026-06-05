c5d3a9c1963dc4adc6210b0eff9f1ee6944fcf62
farhans25-coding <farhan.s25@mhs.usk.ac.id>
2026-06-05 17:40:20 +0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -12,19 +12,14 @@ typedef struct {
     NodeAntrian *rear;
 } Queue;
 
-/* Inisialisasi queue */
 void initQueue(Queue *q);
 
-/* Cek queue kosong */
 int isEmpty(Queue *q);
 
-/* Tambah nasabah ke antrian */
 void enqueue(Queue *q, int idNasabah);
 
-/* Ambil nasabah terdepan */
 int dequeue(Queue *q);
 
-/* Tampilkan isi antrian */
 void displayQueue(Queue *q);
 
 #endif
