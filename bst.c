f8feb7f3747bba59aaa8b4cacdd1af09e4b7ddff
Nad's <nurasysyifa80@gmail.com>
2026-06-05 17:32:41 +0700

    menambahkan implementasi logika penambahan node baru

--- bst.c
+++ bst.c
@@ -12,3 +12,18 @@ NodeNasabah* createNasabahNode(int id, char* nama){
     }
     return newNode;
 }
+//fungsi untuk memasukkan data nasabah baru
+NodeNasabah* insertNasabah (NodeNasabah* root, int id, char* nama){
+    if (root == NULL){
+        return createNasabahNode(id, nama);
+    }
+    //jika id lebih kecil, masuk ke kiri
+    if (id < root->idNasabah){
+        root->left = insertNasabah(root->left, id, nama);
+    }
+    //jika id lebih besar, masuk ke kanan
+    else if (id > root->idNasabah){
+        root->right = insertNasabah(root->right, id, nama);
+    }
+    return root;
+}
